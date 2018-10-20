4d129030c676652582dacd1a6d9cd33915abd783
phugh <peter@phugh.es>
2018-10-20 15:00:52 +0100

    popup.js improvements

--- popup/js/popup.js
+++ popup/js/popup.js
@@ -3,13 +3,9 @@ window.addEventListener('DOMContentLoaded', function() {
     chrome.tabs.sendMessage(tabs[0].id, {from: 'popup', subject: 'data'},
         function(data) {
           if (data && data.PERMA) {
-            // Hide error pane and display data pane
-            document.getElementById('data').classList.remove('hidden');
-            document.getElementById('error').classList.add('hidden');
             // create charts
             createRadarChart(data, document.getElementById('p-radar').getContext('2d'));
             createPieChart(data, document.getElementById('pie').getContext('2d'));
-            // ammend text elements
             // number of elements meeting the block / redaction threshold
             document.getElementById('blocked').textContent = data.filtered;
             // total number of elements scanned
@@ -29,24 +25,33 @@ window.addEventListener('DOMContentLoaded', function() {
             document.getElementById('neg').textContent = data.comp.NEG;
             document.getElementById('pos-total').textContent = data.PERMA.SCORE.POS.toFixed();
             document.getElementById('neg-total').textContent = data.PERMA.SCORE.NEG.toFixed();
-            // domain name
-            const d = document.getElementsByClassName('domain');
-            const dLen = d.length;
-            for (let i = 0; i < dLen; i++) {
-              d[i].textContent = data.domain;
-            }
+            // domain stats
             chrome.storage.sync.get(['domains'], function(result) {
               if (result.domains[data.domain]) {
+                // set domain name text labels
+                const d = document.getElementsByClassName('domain');
+                const dLen = d.length;
+                for (let i = 0; i < dLen; i++) {
+                  d[i].textContent = data.domain;
+                }
+                // calculate and display scores
                 const x = result.domains[data.domain];
                 const pos = sumArr(Object.values(x.PERMA.POS));
                 const neg = sumArr(Object.values(x.PERMA.NEG));
                 document.getElementById('domain-visits').textContent = x.V;
                 document.getElementById('domain-pos-total').textContent = (pos / x.V).toFixed();
                 document.getElementById('domain-neg-total').textContent = (neg / x.V).toFixed();
+                // create graph
                 createDomainChart(x, document.getElementById('d-radar').getContext('2d'));
+                // show card
                 document.getElementById('domain-card').classList.remove('hidden');
+              } else {
+                // @todo add error handling here
               }
             });
+            // Hide error pane and display data pane
+            document.getElementById('error').classList.add('hidden');
+            document.getElementById('data').classList.remove('hidden');
           } else {
             // hide data pane and reveal error pane
             // @todo add an error message if neccessary
@@ -56,6 +61,7 @@ window.addEventListener('DOMContentLoaded', function() {
         });
   });
 
+  // options button
   document.getElementById('opts').addEventListener('click', function() {
     if (chrome.runtime.openOptionsPage) {
       chrome.runtime.openOptionsPage();
@@ -64,10 +70,12 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   });
 
+  // full stats button
   document.getElementById('stats').addEventListener('click', function() {
     window.open(chrome.runtime.getURL('../content/pages/fullstats/stats.html'));
   });
 
+  // refresh page link
   document.getElementById('refresh').addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
